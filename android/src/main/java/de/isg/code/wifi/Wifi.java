08796618cd27727d90af908080e1707c6c692451
Alexander Wodarz <git@alexanderwodarz.de>
2023-01-19 20:56:55 +0100

    Initial commit

---
+++ android/src/main/java/de/isg/code/wifi/Wifi.java
@@ -0,0 +1,11 @@
+package de.isg.code.wifi;
+
+import android.util.Log;
+
+public class Wifi {
+
+    public String echo(String value) {
+        Log.i("Echo", value);
+        return value;
+    }
+}
