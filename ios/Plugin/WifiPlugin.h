5e9864dd190e85a0634ce2ec3c2f90f4a00920e0
Alexander Wodarz <git@alexanderwodarz.de>
2023-01-21 17:39:30 +0100

    Implement NetworkConfiguration in iOS

--- ios/Plugin/WifiPlugin.h
+++ ios/Plugin/WifiPlugin.h
@@ -1,4 +1,11 @@
 #import <UIKit/UIKit.h>
+#import <Capacitor/Capacitor.h>
+
+CAP_PLUGIN(WifiPlugin, "WifiPlugin",
+           CAP_PLUGIN_METHOD(list, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(remove, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(connectWifi, CAPPluginReturnPromise);
+)
 
 //! Project version number for Plugin.
 FOUNDATION_EXPORT double PluginVersionNumber;
--- ios/Plugin/WifiPlugin.swift
+++ ios/Plugin/WifiPlugin.swift
@@ -1,18 +1,57 @@
 import Foundation
 import Capacitor
+import NetworkExtension
 
-/**
- * Please read the Capacitor iOS Plugin Development Guide
- * here: https://capacitorjs.com/docs/plugins/ios
- */
 @objc(WifiPlugin)
 public class WifiPlugin: CAPPlugin {
     private let implementation = Wifi()
-
-    @objc func echo(_ call: CAPPluginCall) {
-        let value = call.getString("value") ?? ""
-        call.resolve([
-            "value": implementation.echo(value)
-        ])
+    
+    @objc func list(_ call: CAPPluginCall) {
+        NEHotspotConfigurationManager().getConfiguredSSIDs(){ ssids in
+            call.resolve(["ssids": ssids])
+        }
+    }
+    
+    @objc func remove(_ call: CAPPluginCall) {
+        let ssid = call.getString("ssid") ?? ""
+        if(ssid.count == 0){
+            call.reject("ssid is empty")
+            return
+        }
+        NEHotspotConfigurationManager().removeConfiguration(forSSID: ssid)
+    }
+    
+    @objc func connectWifi(_ call: CAPPluginCall) {
+        let ssid = call.getString("ssid") ?? ""
+        let password = call.getString("password") ?? ""
+        let isWEP = call.getBool("isWEP") ?? false
+        let once = call.getBool("once") ?? true
+        if(ssid.count == 0){
+            call.reject("ssid is empty")
+            return
+        }
+        let config = NEHotspotConfiguration(ssid: ssid, passphrase: password, isWEP: isWEP)
+        if(once){
+            config.joinOnce = true
+        }
+        let manager = NEHotspotConfigurationManager()
+        manager.apply(config) { (error) in
+            if error != nil {
+                if error?.localizedDescription == "already associated."{
+                    print("Connected")
+                    call.resolve(["connected":true])
+                    return
+                }else{
+                    print("No Connected")
+                    call.reject(error!.localizedDescription)
+                    return
+                }
+            }
+            else {
+                print("Connected")
+                call.resolve(["connected":true])
+                return
+            }
+        }
     }
 }
